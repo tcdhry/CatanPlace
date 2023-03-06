aef04f2158a4fb9016c9b0f7c76d719fc86bda08
tcdhry <hiro2001.ikasamanae.0927ya@gmail.com>
2023-03-06 19:23:56 +0900

    230306 init

---
+++ backend/src/test/java/gxp/krc/catanplace/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gxp.krc.catanplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
